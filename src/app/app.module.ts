86896a26264a332f5b773f52012f0bfeb853bddf
Praveen <praveen@example.com>
2020-04-21 22:07:50 +0530

    socket io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,20 +9,26 @@ import { RegisterComponent } from './users/register/register.component';
 import { FormsModule } from "@angular/forms"
 import { HttpClientModule } from "@angular/common/http";
 import { WeatherComponent } from './weather/weather.component';
-import { HomepageComponent } from './homepage/homepage.component'
+import { HomepageComponent } from './homepage/homepage.component';
+import { ChatComponent } from './chat/chat.component'
+
+const config: SocketIoConfig = { url: 'http://localhost:3005', options: {} };
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     WeatherComponent,
-    HomepageComponent
+    HomepageComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
